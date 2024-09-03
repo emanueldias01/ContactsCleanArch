7568ab298ccd64cefd2177a55b3440ada5240e0c
sabrinadamascenodev <emanueldias01dev@gmail.com>
2024-09-03 16:42:13 -0300

    utiliza dto para registro de contact

--- ContactsCleanArch/src/main/java/br/com/emanueldias01/ContactsCleanArch/infra/controller/ContactController.java
+++ ContactsCleanArch/src/main/java/br/com/emanueldias01/ContactsCleanArch/infra/controller/ContactController.java
@@ -3,9 +3,12 @@ package br.com.emanueldias01.ContactsCleanArch.infra.controller;
 import br.com.emanueldias01.ContactsCleanArch.application.usecases.CreateContact;
 import br.com.emanueldias01.ContactsCleanArch.application.usecases.ListContacts;
 import br.com.emanueldias01.ContactsCleanArch.domain.entities.Contact;
+import br.com.emanueldias01.ContactsCleanArch.domain.entities.ContactFactory;
+import br.com.emanueldias01.ContactsCleanArch.infra.controller.dto.ContactDto;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/contacts")
@@ -14,18 +17,22 @@ public class ContactController {
     private final CreateContact createContact;
     private final ListContacts listContacts;
 
-    public ContactController(CreateContact createContact, ListContacts listContacts) {
+    private final ContactFactory factory;
+
+    public ContactController(CreateContact createContact, ListContacts listContacts, ContactFactory factory) {
         this.createContact = createContact;
         this.listContacts = listContacts;
+        this.factory = factory;
     }
 
     @GetMapping
-    public List<Contact> getAllContacts(){
-        return listContacts.listAllContacts();
+    public List<ContactDto> getAllContacts(){
+        return listContacts.listAllContacts().stream().map(c -> new ContactDto(c.getName(), c.getPhone())).collect(Collectors.toList());
     }
 
     @PostMapping
-    public Contact newContact(@RequestBody Contact contact){
+    public Contact newContact(@RequestBody ContactDto dto){
+        Contact contact = factory.createContact(dto.name(), dto.phone());
         return createContact.createContact(contact);
     }
 }
---
+++ ContactsCleanArch/src/main/java/br/com/emanueldias01/ContactsCleanArch/infra/controller/dto/ContactDto.java
@@ -0,0 +1,6 @@
+package br.com.emanueldias01.ContactsCleanArch.infra.controller.dto;
+
+public record ContactDto(
+        String name, Long phone
+) {
+}
