b57b07a9c3855c12ae8f9a4d8ef3cceaa8f2818b
sabrinadamascenodev <emanueldias01dev@gmail.com>
2024-09-03 15:51:33 -0300

    upload de arquivos

---
+++ ContactsCleanArch/src/main/java/br/com/emanueldias01/ContactsCleanArch/ContactsCleanArchApplication.java
@@ -0,0 +1,13 @@
+package br.com.emanueldias01.ContactsCleanArch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactsCleanArchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactsCleanArchApplication.class, args);
+	}
+
+}
---
+++ ContactsCleanArch/src/test/java/br/com/emanueldias01/ContactsCleanArch/ContactsCleanArchApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.emanueldias01.ContactsCleanArch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactsCleanArchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
